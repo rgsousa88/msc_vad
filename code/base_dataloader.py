ab5f85526f277d04e38778c5eaa9a45641fb1695
rgsousa88 <gadelha.ld@gmail.com>
2025-12-08 04:07:01 -0300

    Including color jitering to pipeline augmentation.

--- code/base_dataloader.py
+++ code/base_dataloader.py
@@ -15,7 +15,7 @@ def simple_pipeline(path: str):
     return images, labels.gpu()
 
 @pipeline_def(num_threads=4, enable_conditionals=True, device_id=0)
-def video_pipe(file_root, shape=(224,224), train=True, device='gpu', sequence_length=10, stride=2, step=10, initial_pref=512):
+def video_pipe(file_root, shape=(224,224), train=True, device='gpu', sequence_length=10, stride=2, step=10, jitter_prob=0.3, initial_pref=512):
     video, labels = fn.readers.video(device=device,
                                     file_root=file_root,
                                     sequence_length=sequence_length,
@@ -24,12 +24,17 @@ def video_pipe(file_root, shape=(224,224), train=True, device='gpu', sequence_le
                                     random_shuffle=True,
                                     initial_fill=initial_pref, name='seq')
     
+    video = fn.resize(video.gpu(), size=shape, device=device)
+
     if train:
         mirror = fn.random.choice(2)
+        do_color_jiter = fn.random.coin_flip(probability=jitter_prob, dtype=types.DALIDataType.BOOL)
+        
+        if do_color_jiter:    
+            video = fn.color_twist(video, brightness=0.3, contrast=0.2, saturation=0.4)
     else:
         mirror = 0
     
-    video = fn.resize(video.gpu(), size=shape, device=device)
     video = fn.crop_mirror_normalize(video, dtype=types.FLOAT, std=[255.0], mirror=mirror, output_layout="FHWC")
     video = fn.transpose(video[1:,:,:,:], perm=[3,0,1,2])
 
