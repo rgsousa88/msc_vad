392fbd1cde8572de4bd7b1021173e3e23565d654
rgsousa88 <gadelha.ld@gmail.com>
2025-12-21 17:10:07 -0300

    Including dataloader for frames from videos.

--- code/base_dataloader.py
+++ code/base_dataloader.py
@@ -15,6 +15,35 @@ def simple_pipeline(path: str):
     return images, labels.gpu()
 
 @pipeline_def(num_threads=4, enable_conditionals=True, device_id=0)
+def video_seq_pipe(file_root, shape=(224,224), train=True, device='gpu', sequence_length=10, fstride=2, cstride=20, change_color_prob=0.3):
+    video = fn.readers.sequence(file_root=file_root,
+                                sequence_length=sequence_length,
+                                stride=fstride,
+                                step=cstride,
+                                name='seq')
+    
+    video = fn.resize(video.gpu(), size=shape, device=device)
+    
+    if train:
+        mirror = fn.random.choice(2)
+    else:
+        change_color_prob = 0.0
+        mirror = 0
+    
+    video = fn.crop_mirror_normalize(video, dtype=types.FLOAT, std=[255.0], mirror=mirror, output_layout="FHWC")       
+    
+    do_color_changes = fn.random.coin_flip(probability=change_color_prob, dtype=types.DALIDataType.BOOL)
+
+    if do_color_changes:
+        randBrightness = fn.uniform(range=(0.5,1.5))
+        randContrast = fn.uniform(range=(0.5,1.5))
+        video = fn.brightness_contrast(video, brightness=randBrightness, contrast=randContrast)
+    
+    video = fn.transpose(video[1:,:,:,:], perm=[3,0,1,2])
+
+    return video
+
+@pipeline_def(num_threads=4, enable_conditionals=True, device_id=0)
 def video_pipe(file_root, shape=(224,224), train=True, device='gpu', sequence_length=10, stride=2, step=10, change_color_prob=0.3, initial_pref=512):
     video, labels = fn.readers.video(device=device,
                                     file_root=file_root,
